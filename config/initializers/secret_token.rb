25d8c44a58a305aa473f7661a9f7c27d105d377f
Ossian MacDonald <ossianmacdonald@gmail.com>
2013-09-23 18:57:50 -0700

    git commit -m "added bootstrap and applied layout to the page" git commit -m"" git commit -m git commit -m""
    it commit -m 'Type your commit message here'
    git commit -m "added bootstrap"
    git commit --amend "added bootstap"
    git commit --amend -m "added bootsrap"
    git push origin master
    i git commit -m "added bootstrap"
    q
    :q.
    :q
    git push origin master
    ":q"
    i
    "added bootstrap and applied layout"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'edc3263a4a1b4604e147bb3e1a7fc6304567df74a5bd54ecdf78c0022714a76e1907ea30df7ef7320ae50e309842edc409079ffd6de44287044a7c4f00bb3648'
